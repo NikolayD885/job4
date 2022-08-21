3393a88778957499f1c704cdbc5e7dcd3dae9ca7
NikolayD <kolik885@ya.ru>
2022-08-22 00:06:12 +0700

    Программа к заданию №25

---
+++ 025/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+
+Console.WriteLine("Введите число которое нужно возвести в степень");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите степень в которую нужно возвести число");
+int number2 = int.Parse(Console.ReadLine()!);
+
+int AtoB(int number1, int number2){
+
+   var result = number1;
+ 
+   for(int i = 2; i <= number2; i++)
+     result *= number1;
+
+    return result;
+}
+
+var result = AtoB(number1, number2);
+Console.WriteLine("\n" + "Ответ " + result);
