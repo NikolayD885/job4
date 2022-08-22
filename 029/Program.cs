4344df2074fbd59dcf97d705fe954b4bf9c2f61a
NikolayD <kolik885@ya.ru>
2022-08-23 00:18:50 +0700

    программа к заданию 29

---
+++ 029/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа которая задает массив из 8 элементов и выводит их на экран
+
+Console.Clear();
+
+int[] CreateArray()
+  {
+    int[] array = new int[8];
+    var random = new Random();
+
+    for (int i = 0; i < 8; i++)
+        array[i] = random.Next(0, 99);
+
+    return array;
+  }
+
+  var array = CreateArray();
+
+  for (int i = 0; i < 8; i++)
+    Console.Write(" " +array[i]);
