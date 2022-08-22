fe41465c55960cc24b3ecae0a285a06505a79324
NikolayD <kolik885@ya.ru>
2022-08-22 23:38:35 +0700

    программа к заданию 27

---
+++ 027/Program.cs
@@ -0,0 +1,42 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int Sum(int number)
+{
+    for(int i = 1, sum = 0;  ; i++)
+     {
+     int a = number %10;
+
+     sum = sum + a;
+
+     number /= 10;
+
+     if (number == 0)
+    
+      return sum;
+     }
+    
+}
+ var result = Sum(number);
+
+ Console.WriteLine("\n" + "Сумма = " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
